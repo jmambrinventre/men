1d3a5498cc258ea852bce18a71c60a4a5ee073bc
jmambrinventre <jmambrinventre@gmail.com>
2023-02-10 15:34:34 -0300

    api bicicletas

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -4,5 +4,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attibution: '&copy; <a href="https://www.openstreetmap.org/copyright">Open</a> contributors'
 }).addTo(map);
 
-L.marker([-27.450888, -58.986478]).addTo(map);
-L.marker([-27.433185780483903, -58.967382870152946]).addTo(map);
+// L.marker([-27.450888, -58.986478]).addTo(map);
+// L.marker([-27.433185780483903, -58.967382870152946]).addTo(map);
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map)
+        });
+    }
+})
+
