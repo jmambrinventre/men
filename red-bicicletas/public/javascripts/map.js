4a847853527c2a4b33b755886f994c3d63662ebd
jmambrinventre <jmambrinventre@gmail.com>
2023-02-10 19:24:32 -0300

    week 1 - u

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -4,8 +4,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attibution: '&copy; <a href="https://www.openstreetmap.org/copyright">Open</a> contributors'
 }).addTo(map);
 
-// L.marker([-27.450888, -58.986478]).addTo(map);
-// L.marker([-27.433185780483903, -58.967382870152946]).addTo(map);
+L.marker([-27.450888, -58.986478]).addTo(map);
+L.marker([-27.433185780483903, -58.967382870152946]).addTo(map);
 
 $.ajax({
     dataType: "json",
