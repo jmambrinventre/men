77e44a7c30d76dd19fde485639b127e96c4aa653
jmambrinventre <jmambrinventre@gmail.com>
2023-02-10 09:08:00 -0300

    adding map and others

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -1,9 +1,8 @@
-var map = L.map('main_map').setView([-34.6012424, -58.3861497], 13);
+var map = L.map('main_map').setView([-27.450888, -58.986478], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attibution: '&copy; <a href="https://www.openstreetmap.org/copyright">Open</a> contributors'
 }).addTo(map);
 
-L.marker([-34.596932,-58.3808287]).addTo(map);
-L.marker([-34.596924,-58.3808297]).addTo(map);
-L.marker([-34.596964,-58.3808267]).addTo(map);
+L.marker([-27.450888, -58.986478]).addTo(map);
+L.marker([-27.433185780483903, -58.967382870152946]).addTo(map);
