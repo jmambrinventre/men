ee194d1e594226f7670a6d9fd4802e1e38ed8917
jmambrinventre <jmambrinventre@gmail.com>
2023-02-28 15:45:11 -0300

    bicicleta model test

---
+++ red-bicicletas/spec/models/bicicleta_test.spec.js
@@ -0,0 +1,49 @@
+var Bicicleta = require('../../models/bicicleta');
+
+describe("Bicicleta", function(){
+    beforeEach(function(){
+        // Agregamos dos bicicletas a la lista de bicicletas
+        Bicicleta.allBicis = [];
+        var a = new Bicicleta(1, 'rojo', 'urbana', [-34.6012414, -58.3861497]);
+        var b = new Bicicleta(2, 'blanca', 'urbana', [-34.596932, -58.3808287]);
+        Bicicleta.add(a);
+        Bicicleta.add(b);
+    });
+
+    describe("Bicicleta.add", () => {
+        it("Agrega una bicicleta a la lista de bicicletas", () => {
+            var newBici = new Bicicleta(3, 'verde', 'urbana', [-34.5950142, -58.3829167]);
+            Bicicleta.add(newBici);
+            expect(Bicicleta.allBicis.length).toBe(3);
+            expect(Bicicleta.allBicis[2].id).toBe(3);
+        });
+    });
+
+    describe("Bicicleta.findbyId", () => {
+        it("Busca una bicicleta por su id y la devuelve", () => {
+            var bici = Bicicleta.findbyId(1);
+            expect(bici).toBeDefined();
+            expect(bici.id).toBe(1);
+            expect(bici.color).toBe('rojo');
+        });
+
+        it("Devuelve un error si la bicicleta no existe", () => {
+            expect(() => {Bicicleta.findbyId(10)}).toThrowError("No existe una bicicleta con el id 10");
+        });
+    });
+
+    describe("Bicicleta.removeById", () => {
+        it("Remueve una bicicleta de la lista de bicicletas", () => {
+            var bici = Bicicleta.findbyId(1);
+            expect(Bicicleta.allBicis.length).toBe(2);
+
+            Bicicleta.removeById(1);
+            expect(Bicicleta.allBicis.length).toBe(1);
+            expect(() => {Bicicleta.findbyId(1)}).toThrowError("No existe una bicicleta con el id 1");
+        });
+
+        it("Devuelve un error si la bicicleta no existe", () => {
+            expect(() => {Bicicleta.removeById(10)}).toThrowError("No existe una bicicleta con el id 10");
+        });
+    });
+});
